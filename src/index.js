1e22a50a6658b7fa59afed511a6113860b29a539
Elisabeth Garcia <lisgarciae@gmail.com>
2023-04-12 20:15:22 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+//const { locationInfo } = require("jsdom/lib/jsdom/living/helpers/internal-constants");
+
+document.addEventListener("DOMContentLoaded", (event) => {});
+
+const tasks = document.querySelector("#tasks")
+const submitBtn=document.querySelector("#submit")
+const userImput= document.querySelector("#new-task-description")
+
+
+function addNewTask (text) {
+  const taskItem= document.createElement("li");
+  const littleBtn =document.createElement("span")
+  littleBtn.classList.add("littleBtn")
+  taskItem.textContent= userImput.value
+  tasks.append(taskItem, littleBtn)
+}
+
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  addNewTask()  
+})
+
+
+
+//needs: 1. type a task into input field, 
+// 2. click submit button
+//3. task string input appears in the DOM 
+// Utilizes: HTML <form> element, <input> element, 
+  // <type> attributes (type="text") and (type="submit")
